8e41ff420d5edd2ae7410f293cf0896f5bfb7d67
agatatok <agattok260@student.polsl.pl>
2020-04-03 22:41:18 +0200

    Implemented Sorter with main method

--- algorytmy/src/main/java/algorithm/Sorter.java
+++ algorytmy/src/main/java/algorithm/Sorter.java
@@ -1,4 +1,22 @@
 package algorithm;
 
+import algorithm.sort.BubbleSorter;
+
+import java.util.Arrays;
+
 public class Sorter {
+
+    public static void main(String[] args) {
+
+        int[] arrayForSorting = ArrayGenerator.deliverRandomArray(15);
+        System.out.println("Array before sorting:");
+        showTheElements(arrayForSorting);
+        BubbleSorter.sort(arrayForSorting);
+        System.out.println("Array after sorting:");
+        showTheElements(arrayForSorting);
+    }
+
+    private static void showTheElements(int[] arrayToBeProcessed) {
+        Arrays.stream(arrayToBeProcessed).forEach(System.out::println);
+    }
 }
