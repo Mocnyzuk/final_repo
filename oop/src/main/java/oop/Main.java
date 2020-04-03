e5d8ba280eacfae15cc3d25adb0dc0745cd7c71e
agatatok <agattok260@student.polsl.pl>
2020-04-03 23:32:06 +0200

    Pizza1 object created in main

--- oop/src/main/java/oop/Main.java
+++ oop/src/main/java/oop/Main.java
@@ -1,7 +1,14 @@
 package oop;
 
+import oop.domain.Cheese;
+import oop.domain.Pizza;
+import oop.domain.Sauce;
+
 public class Main {
     public static void main(String[] args) {
-
+        Pizza pizza1 = Pizza.PizzaBuilder.aPizza(30,"Margherita", Sauce.TOMATO, Cheese.MOZZARELLA)
+                .withExtraCheese(true)
+                .withMushroom(true)
+                .build();
     }
 }
