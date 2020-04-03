5dc5d86d1a833f67615b81808f000b38df8598bb
TheDankMatter <piotriwo@gmail.com>
2020-04-03 12:25:06 +0200

    Updated fibonacciIter Test

--- Piotr_Szymanski/algorytmy/src/test/java/pl/accenture/algorytmy/FibonacciTest.java
+++ Piotr_Szymanski/algorytmy/src/test/java/pl/accenture/algorytmy/FibonacciTest.java
@@ -23,4 +23,18 @@ class FibonacciTest {
         // Then
         assertArrayEquals(expectedSequence, outputSequence);
     }
+
+    @Test
+    @DisplayName("fibonacciIter test")
+    void fibonacciIter() {
+        // Given
+        int expectedSequence[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946};
+        // When
+        int outputSequence[] = new int[expectedSequence.length];
+        for (int i = 0; i < expectedSequence.length; i++) {
+            outputSequence[i] = (Fibonacci.fibonacciIter(i).intValue());
+        }
+        // Then
+        assertArrayEquals(expectedSequence, outputSequence);
+    }
 }
