df145c02f1c0252ab6292b1d5199a99c64958e26
TheDankMatter <piotriwo@gmail.com>
2020-04-03 11:12:01 +0200

    Added test for fibonacciRecursion

---
+++ Piotr_Szymanski/algorytmy/src/test/java/pl/accenture/algorytmy/FibonacciTest.java
@@ -0,0 +1,15 @@
+package pl.accenture.algorytmy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciTest {
+
+    @Test
+    @DisplayName("fibonacciRecursion test")
+    void fibonacciRecursion() {
+        fail();
+    }
+}
