393c6f43b6bab8a394d7485c0d0c4c42febfb8e1
TheDankMatter <piotriwo@gmail.com>
2020-04-03 14:41:40 +0200

    Added test for getDistance & getFuelNeeds

---
+++ Piotr_Szymanski/OOP/src/test/java/pl/accenture/oop/PlaneTest.java
@@ -0,0 +1,16 @@
+package pl.accenture.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaneTest {
+
+    @Test
+    void getFuelNeeds() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+}
