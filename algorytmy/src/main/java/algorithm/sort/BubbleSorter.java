67575d43ad22efdb8b05e76c01416c9d703a1bf7
agatatok <agattok260@student.polsl.pl>
2020-04-03 22:29:50 +0200

    Added methods implementation in BubbleSorter

--- algorytmy/src/main/java/algorithm/sort/BubbleSorter.java
+++ algorytmy/src/main/java/algorithm/sort/BubbleSorter.java
@@ -1,4 +1,20 @@
 package algorithm.sort;
 
 public class BubbleSorter {
+
+    public static void sort(int[] arrayToSort) {
+        for (int i = 0; i < arrayToSort.length-1; i++) {
+            for (int j = 0; j < arrayToSort.length - i - 1; j++) {
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    swapElements(arrayToSort, j, j + 1);
+                }
+            }
+        }
+    }
+
+    static void swapElements(int[] arrayToSort, int indexOfElement1, int indexOfElement2) {
+        int temp=arrayToSort[indexOfElement1];
+        arrayToSort[indexOfElement1]=arrayToSort[indexOfElement2];
+        arrayToSort[indexOfElement2]=temp;
+    }
 }
