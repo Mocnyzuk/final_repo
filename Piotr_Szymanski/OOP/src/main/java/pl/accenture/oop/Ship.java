067f3b06e34e8d2fcf475412b63b10a74c6c7d6f
TheDankMatter <piotriwo@gmail.com>
2020-04-03 15:06:58 +0200

    Implemented Ship class

--- Piotr_Szymanski/OOP/src/main/java/pl/accenture/oop/Ship.java
+++ Piotr_Szymanski/OOP/src/main/java/pl/accenture/oop/Ship.java
@@ -1,4 +1,25 @@
 package pl.accenture.oop;
 
-public class Ship {
+public class Ship extends Vehicle implements Sailing {
+
+    @Override
+    public double getFuelNeeds() {
+        return 30;
+    }
+
+    @Override
+    public double getDistance() {
+        return 300;
+    }
+
+    @Override
+    public void dock() {
+        System.out.println("Docking");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("The boat stops");
+    }
+
 }
