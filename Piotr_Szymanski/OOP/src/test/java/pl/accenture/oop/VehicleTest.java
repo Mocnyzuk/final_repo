eb1b9d814709fbb0044d53bbc90e335ea8010af9
TheDankMatter <piotriwo@gmail.com>
2020-04-03 14:18:27 +0200

    Added test for getFuelNeeds()

---
+++ Piotr_Szymanski/OOP/src/test/java/pl/accenture/oop/VehicleTest.java
@@ -0,0 +1,30 @@
+package pl.accenture.oop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    @DisplayName("getFuelNeeds test")
+    void getFuelNeeds() {
+        // Given
+        Vehicle vehicle = new Vehicle();
+        // When
+        // Then
+        assertEquals(7, vehicle.getFuelNeeds());
+    }
+
+    @Test
+    @DisplayName("getDistance test")
+    void getDistance() {
+    }
+
+    @Test
+    @DisplayName("calculateFuelConsumption test")
+    void calculateFuelConsumption() {
+    }
+
+}
