1a967bd1362be2cb9d8f460e8ee7890e0fb6998c
TheDankMatter <piotriwo@gmail.com>
2020-04-03 15:51:29 +0200

    Added Factory design pattern

--- Piotr_Szymanski/OOP/src/main/java/pl/accenture/MainFactory.java
+++ Piotr_Szymanski/OOP/src/main/java/pl/accenture/MainFactory.java
@@ -1,4 +1,23 @@
 package pl.accenture;
 
+import pl.accenture.oop.*;
+
 public class MainFactory {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Implementation of factory design pattern
+    public Vehicle chooseVehicle(int speed, int cargo ) {
+        if (cargo > 1000) {
+            return new Ship();
+        } else if (speed > 300 && cargo > 300) {
+            return new Plane();
+        } else if (speed < 300 && cargo < 10) {
+            return new RaceCar();
+        } else {
+            return new Car();
+        }
+    }
 }
