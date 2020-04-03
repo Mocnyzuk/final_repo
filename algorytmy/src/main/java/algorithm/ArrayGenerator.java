15629d461900468ea2d291b8d1f58be25b6d284f
agatatok <agattok260@student.polsl.pl>
2020-04-03 22:40:50 +0200

    Implemented methods in ArrayGenerator

--- algorytmy/src/main/java/algorithm/ArrayGenerator.java
+++ algorytmy/src/main/java/algorithm/ArrayGenerator.java
@@ -1,7 +1,27 @@
 package algorithm;
+import java.util.Random;
 
+/**
+ * ArrayGenerator delivers the array filled with random integers range from 0 to 100.
+ */
 public class ArrayGenerator {
-    public static void main(String[] args) {
 
+    static final Random intGenerator = new Random();
+    public static int[] arrayOfIntegers;
+
+    private ArrayGenerator() {
     }
+
+    /**
+     * @param arraySize the count of elements in generated array
+     * */
+    public static int[] deliverRandomArray(int arraySize)
+    {
+        arrayOfIntegers = new int[arraySize];
+        for (int i = 0; i < arrayOfIntegers.length; i++) {
+            arrayOfIntegers[i] = intGenerator.nextInt(100);
+        }
+        return arrayOfIntegers;
+    }
+
 }
