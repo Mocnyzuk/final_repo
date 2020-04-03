ad9c80ef9e8d97055626eb37a9121e8a4e0c7ccb
TheDankMatter <piotriwo@gmail.com>
2020-04-03 12:31:06 +0200

    Added tests for fibonacciMemoization method

--- Piotr_Szymanski/algorytmy/src/test/java/pl/accenture/algorytmy/FibonacciTest.java
+++ Piotr_Szymanski/algorytmy/src/test/java/pl/accenture/algorytmy/FibonacciTest.java
@@ -37,4 +37,18 @@ class FibonacciTest {
         // Then
         assertArrayEquals(expectedSequence, outputSequence);
     }
+
+    @Test
+    @DisplayName("fibonacciMemoization test")
+    void fibonacciMemoization() {
+        // Given
+        int expectedSequence[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946};
+        // When
+        int outputSequence[] = new int[expectedSequence.length];
+        for (int i = 0; i < expectedSequence.length; i++) {
+            outputSequence[i] = (Fibonacci.fibonacciMemoization(i).intValue());
+        }
+        // Then
+        assertArrayEquals(expectedSequence, outputSequence);
+    }
 }
