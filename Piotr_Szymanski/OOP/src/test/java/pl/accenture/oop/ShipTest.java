484e7fd02481a7f52eabf10125fb9974fdf1b160
TheDankMatter <piotriwo@gmail.com>
2020-04-03 15:06:37 +0200

    Added Ship tests

---
+++ Piotr_Szymanski/OOP/src/test/java/pl/accenture/oop/ShipTest.java
@@ -0,0 +1,31 @@
+package pl.accenture.oop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipTest {
+
+    @Test
+    @DisplayName("getFuelNeeds test")
+    void getFuelNeeds() {
+        // Given
+        Ship ship = new Ship();
+        // When
+        double fuelNeeds = ship.getFuelNeeds();
+        // Then
+        assertEquals(30, fuelNeeds);
+    }
+
+    @Test
+    @DisplayName("getDistance test")
+    void getDistance() {
+        // Given
+        Ship ship = new Ship();
+        // When
+        double distance = ship.getDistance();
+        // Then
+        assertEquals(300, distance);
+    }
+}
