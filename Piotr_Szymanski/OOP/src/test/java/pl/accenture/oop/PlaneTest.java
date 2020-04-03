a87128eb8d44a121d1f41a7112ad9a34a854af2b
TheDankMatter <piotriwo@gmail.com>
2020-04-03 14:51:41 +0200

    Added getFuelNeeds test

--- Piotr_Szymanski/OOP/src/test/java/pl/accenture/oop/PlaneTest.java
+++ Piotr_Szymanski/OOP/src/test/java/pl/accenture/oop/PlaneTest.java
@@ -1,5 +1,6 @@
 package pl.accenture.oop;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlaneTest {
 
     @Test
+    @DisplayName("getFuelNeeds test")
     void getFuelNeeds() {
+        // Given
+        Plane plane = new Plane();
+        // When
+        double fuelNeeds = plane.getFuelNeeds();
+        // Then
+        assertEquals(100, fuelNeeds);
+
+
     }
 
     @Test
+    @DisplayName("getDistance test")
     void getDistance() {
+
     }
 }
