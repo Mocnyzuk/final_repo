de7e8b0668391064f8ba0541e75d45f9073f16b9
agatatok <agattok260@student.polsl.pl>
2020-04-04 12:25:09 +0200

    Implemented interface

--- Agata_T/functional/src/main/java/domain/PredicateAndCompareInterface.java
+++ Agata_T/functional/src/main/java/domain/PredicateAndCompareInterface.java
@@ -1,4 +1,27 @@
 package domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@FunctionalInterface
 public interface PredicateAndCompareInterface {
+    List<Person> filterAndSort(List<Person> people);
+
+    PredicateAndCompareInterface useFirstPredAndCompar = people -> people
+            .stream()
+            .filter(Person.ifFirstNameEndsWithA)
+            .sorted(Person.sortByFirstName)
+            .collect(Collectors.toList());
+
+    PredicateAndCompareInterface useSecondPredAndCompar = people -> people
+            .stream()
+            .filter(Person.ifPersonGotBlueEyes)
+            .sorted(Person.sortByLastName.reversed())
+            .collect(Collectors.toList());
+
+    PredicateAndCompareInterface useThirdPredAndCompar = people -> people
+            .stream()
+            .filter(Person.ifPersonIsAdult)
+            .sorted(Person.sortByAge.thenComparing(Person.sortByLastName))
+            .collect(Collectors.toList());
 }
