ed5d26da90dfb5ded5fa1cfbf06f33704e39dde0
agatatok <agattok260@student.polsl.pl>
2020-04-04 18:38:55 +0200

    Spring done

--- Agata_T/spring/src/test/java/com/example/controller/WebControllerTest.java
+++ Agata_T/spring/src/test/java/com/example/controller/WebControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -34,6 +35,17 @@ class WebControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/")
                 .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnText() throws Exception {
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/results")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
